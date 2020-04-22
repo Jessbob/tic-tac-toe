74893005da151546e3d5d4eecefbe052deec5e2c
JessBob <54863103+Jessbob@users.noreply.github.com>
2020-04-22 16:29:46 -0500

    defined a few functions and designed the board

---
+++ tic-tac-toe.py
@@ -0,0 +1,17 @@
+board = [" " for x in range(10)]
+
+
+def insertLetter(letter, pos):
+    board[pos] = letter
+
+
+def spaceIsFree(pos):
+    return board[pos] == " "
+
+
+def printBoard(board):
+    print(" " + board[1] + " | " + board[2] + " | " + board[3])
+    print("-----------")
+    print(" " + board[4] + " | " + board[5] + " | " + board[6])
+    print("-----------")
+    print(" " + board[7] + " | " + board[8] + " | " + board[9])
